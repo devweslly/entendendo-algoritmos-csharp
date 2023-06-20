d9509fed14c7464c4f0635558303f61742133634
DevWeslly <devweslly@gmail.com>
2023-06-20 00:15:41 -0300

    [Entendendo-Algoritmos] Ordenação por Seleção

---
+++ Entendendo-Algoritmos/02_Ordenacao_Por_Selecao/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * Ordenação por seleção
+         */
+
+        List<int> arr = new List<int> { 5, 3, 6, 2, 10 };
+        Console.WriteLine(string.Join(", ", SelectionSort(arr)));
+    }
+
+    private static int[] SelectionSort(List<int> arr)
+    {
+        int[] novoArr = new int[arr.Count()];
+
+        for (int indice = 0; indice < novoArr.Count(); indice++)
+        {
+            int indiceMenorValor    = EncontrarMenor(arr);
+            novoArr[indice]         = arr[indiceMenorValor];
+            arr.Remove(arr[indiceMenorValor]);
+        }
+
+        return novoArr;
+    }
+
+    private static int EncontrarMenor(List<int> arr)
+    {
+        int menorValor          = arr[0];
+        int indiceMenorValor    = 0;
+
+        for (int indice = 0; indice < arr.Count; indice++)
+        {
+            if (arr[indice] < menorValor)
+            {
+                menorValor          = arr[indice];
+                indiceMenorValor    = indice;
+            }
+        }
+        
+        return indiceMenorValor;
+    }
+}
