435bf390e41e9cd0555e369c1983e3d9f88e5f8e
DevWeslly <devweslly@gmail.com>
2023-08-26 17:05:10 -0300

    [Entendendo-Algoritmos] Tabelas Hash e Grafo

---
+++ Entendendo-Algoritmos/05_Hash_Tables/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    private static Dictionary<string, bool> _voto = new Dictionary<string, bool>();
+    private static void Main(string[] args)
+    {
+
+        //MostraPrecoMecearia();
+                
+        ChacandoVoto("Weslly");        
+        ChacandoVoto("João");        
+        ChacandoVoto("José");        
+    }
+
+    private static void ChacandoVoto(string eleitor)
+    {
+        if (_voto.ContainsKey(eleitor))
+        {
+            Console.WriteLine("Vai embora, carolho!");
+        }
+        else
+        {
+            _voto.Add(eleitor, true);
+            Console.WriteLine("Vota logo, porra!");
+        }
+    }
+
+    private static void MostraPrecoMecearia()
+    {
+        Dictionary<string, double> book = new Dictionary<string, double>
+        {
+            {"Suco", 7.90 },
+            {"Pão", 1.90 }
+        };
+
+        book.Add("maça", 0.67);
+        book.Add("leite", 1.49);
+        book.Add("abacate", 0.90);
+
+        foreach (KeyValuePair<string, double> item in book)
+        {
+            Console.WriteLine($"{item.Key} : {item.Value}");
+        }
+    }
+}
---
+++ Entendendo-Algoritmos/06_Grafo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class Program
+{
+    private static Dictionary<string, string[]> _grafo = new Dictionary<string, string[]>();
+    private static void Main(string[] args)
+    {
+        _grafo.Add("voce", new[] { "alice", "bob", "claire" });
+        _grafo.Add("bob", new[] { "anuj", "peggy" });
+        _grafo.Add("alice", new[] { "peggy" });
+        _grafo.Add("claire", new[] { "thom", "jonny" });
+        _grafo.Add("anuj", Array.Empty<string>());
+        _grafo.Add("peggy", Array.Empty<string>());
+        _grafo.Add("thom", Array.Empty<string>());
+        _grafo.Add("jonny", Array.Empty<string>());
+        Pesquisa("voce");
+    }
+
+    private static bool Pesquisa(string nome)
+    {
+        // Insere amigos na fila
+        Queue<string> pesquisaFila  = new Queue<string>(_grafo[nome]);
+        List<string> verificado     = new List<string>();
+
+        while (pesquisaFila.Any())
+        {
+            string pessoa = pesquisaFila.Dequeue();
+            if (!verificado.Contains(pessoa))
+            {
+                if (verificaPessoaVendedor(pessoa))
+                {
+                    Console.WriteLine($"{pessoa} é um vendedor de manga");
+                    return true;
+                }
+                else
+                {
+                    // insere amigos da pessoa na fila
+                    pesquisaFila = new Queue<string>(pesquisaFila.Concat(_grafo[pessoa]));
+                    verificado.Add(pessoa);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static bool verificaPessoaVendedor(string nome)
+    {
+        return nome.EndsWith("m");
+    }
+}
